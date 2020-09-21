369dc01d17522ac32676ac00ca180b6a6dde750e
Fran Nav <frannavmart@gmail.com>
2020-09-21 12:45:54 +0100

    Added CoinDetailScreen and button to navigate

--- src/components/coins/CoinsScreen.js
+++ src/components/coins/CoinsScreen.js
@@ -1,15 +1,46 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Pressable, StyleSheet } from 'react-native';
 
 
 class CoinsScreen extends Component {
+  handlePress = () => {
+    console.log("Go to details", this.props);
+    
+    this.props.navigation.navigate('CoinDetail');
+  }
+
   render() {
     return (
-      <View>
-        <Text>Coins Screen</Text>
+      <View style={styles.container}>
+        <Text style={styles.titleText} >Coins Screen</Text>
+        <Pressable style={styles.btn} onPress={this.handlePress}>
+          <Text style={styles.btnText}> Go to Details </Text>
+        </Pressable>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "red",
+  },
+  titleText: {
+    color: "#fff",
+    textAlign: "center"
+  },
+  btn: {
+    padding: 8,
+    backgroundColor: "blue",
+    borderRadius: 8,
+    margin: 16
+  },
+  btnText: {
+    color: "#fff",
+    textAlign: "center"
+  }
+
+});
 
 export default CoinsScreen;
