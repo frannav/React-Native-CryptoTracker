8dd2deeda8149a444438a06c405c4bfd6bb1cda7
Fran Nav <frannavmart@gmail.com>
2020-09-30 15:47:56 +0100

    Add favorite list

--- src/components/favorites/FavoritesScreen.js
+++ src/components/favorites/FavoritesScreen.js
@@ -1,14 +1,72 @@
 import React, { Component } from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, FlatList, StyleSheet } from 'react-native';
 import FavoritesEmptyState from './FavoritesEmptyState';
+import CoinsItem from 'cryptoTracker/src/components/coins/CoinsItem';
 import Colors from 'cryptoTracker/src/res/colors';
+import Storage from 'cryptoTracker/src/libs/storage';
 
 
 class FavoritesScreen extends Component {
+
+    state = {
+        favorites: []
+    }
+
+    getFavorites = async () => {
+        try {
+            const allKeys = await Storage.instance.getAllKeys();
+
+            const keys = allKeys.filter((key) => key.includes("favorite-"));
+
+            const favsArray = await Storage.instance.multiGet(keys);
+
+            const favorites = favsArray.map((item) => JSON.parse(item[1]));
+
+            console.log("JEJEJEJEJ: ", favorites);
+
+            this.setState({ favorites })
+
+        } catch (err) {
+            console.log("Get favorites err: ", err);
+        }
+    }
+
+    handlePress = (coin) => {
+        this.props.navigation.navigate("CoinDetail", { coin });
+    }
+
+    componentDidMount() {
+        this.getFavorites();
+        this.props.navigation.addListener("focus", this.getFavorites);
+    }
+
+    componentWillUnmount() {
+        this.props.navigation.removeListener("focus", this.getFavorites);
+    }
+
     render () {
+
+        const { favorites } = this.state
+
         return (
             <View style={styles.container}>
-                <FavoritesEmptyState />
+                { favorites.length == 0 ? 
+                    <FavoritesEmptyState />
+                : null
+                }
+                {
+                    favorites.length > 0 ? 
+                    <FlatList 
+                        data={favorites}
+                        renderItem={({ item }) =>
+                            <CoinsItem 
+                                item={item} 
+                                onPress={() => this.handlePress(item)}
+                            />
+                        }
+                    />
+                    : null
+                }
             </View>
         );
     }
